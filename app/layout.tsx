325bf1155823199be3981a850c1b595b995a590d
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-06-06 16:50:25 +0400

    change font and background
    Import poppins font from google
    Change html background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Poppins } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
 
 export const metadata: Metadata = {
   title: "Home Service App",
@@ -16,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${poppins.variable}`}>
     <body>
       <Navbar/>
       <main className="relative overflow-hidden">

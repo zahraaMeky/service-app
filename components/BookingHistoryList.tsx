25fff1750de004aae706020fbee6fbede7f76460
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-06-24 21:15:38 +0400

    Update BookingHistoryList.tsx

--- components/BookingHistoryList.tsx
+++ components/BookingHistoryList.tsx
@@ -1,54 +1,69 @@
 "use client"
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
-import Image from 'next/image'
+import Image from 'next/image';
 import GlobalApi from "@/app/api/GlobalApi";
-import { Calendar, Clock, MapIcon, MapPin, User } from "lucide-react";
+import { Calendar, Clock, MapPin, User } from "lucide-react";
+
+type Booking = {
+  businessList: {
+    name: string;
+    image: { url: string }[];
+    contactPerson: string;
+    address: string;
+  };
+  date: string;
+  time: string;
+};
+
 const BookingHistoryList = () => {
-    const {data}=useSession()
-    const [bookingHistory,setBookingHistory]=useState([]);
+  const { data } = useSession();
+  const [bookingHistory, setBookingHistory] = useState<Booking[]>([]);
 
-    useEffect(()=>{
-        data&&GetUserBookingHistory();
-      },[data])
-    
-    const GetUserBookingHistory=()=>{
-        GlobalApi.GetUserBookingHistory(data?.user?.email).then(resp=>{
-            setBookingHistory(resp.bookings);
-        })
-      }
+  useEffect(() => {
+    data && GetUserBookingHistory();
+  }, [data]);
+
+  const GetUserBookingHistory = () => {
+    GlobalApi.GetUserBookingHistory(data?.user?.email).then(resp => {
+      setBookingHistory(resp.bookings);
+    });
+  };
+
   return (
-    <div className="grid grid-cols-1  md:grid-cols-2">
+    <div className="grid grid-cols-1 md:grid-cols-2">
       {bookingHistory.map((booking, index) => (
-        <div key={index} className="flex  gap-4 rounded-lg p-4  border mb-5">
-
-        {booking?.businessList?.name &&
-            <Image src={booking.businessList.image[0].url}
-                    alt='icon'
-                    width={120}
-                    height={120}
-                    className="rounded-lg object-cover"
+        <div key={index} className="flex gap-4 rounded-lg p-4 border mb-5">
+          {booking?.businessList?.name &&
+            <Image 
+              src={booking.businessList.image[0].url}
+              alt='icon'
+              width={120}
+              height={120}
+              className="rounded-lg object-cover"
             />
-        }
-        <div className="flex flex-col gap-2">
+          }
+          <div className="flex flex-col gap-2">
             <h2 className="font-bold">{booking?.businessList?.name}</h2>
-            <h2  className="flex  gap-2 text-purple-400"><User/>{booking?.businessList?.contactPerson}</h2>
-            <h2 className="flex  gap-2 text-gray-500"><MapPin className="text-purple-400"/>{booking?.businessList?.address}</h2>
-            <h2 className="flex  gap-2 text-gray-500">
-                Service on : <Calendar className="text-purple-400"/>
-                <span className="text-black">{booking.date}</span>
+            <h2 className="flex gap-2 text-purple-400">
+              <User />{booking?.businessList?.contactPerson}
             </h2>
-            <h2 className="flex  gap-2 text-gray-500">
-            Service on : <Clock className="text-purple-400"/>
-                <span className="text-black">{booking.time}</span>
+            <h2 className="flex gap-2 text-gray-500">
+              <MapPin className="text-purple-400" />{booking?.businessList?.address}
             </h2>
-
+            <h2 className="flex gap-2 text-gray-500">
+              Service on: <Calendar className="text-purple-400" />
+              <span className="text-black">{booking.date}</span>
+            </h2>
+            <h2 className="flex gap-2 text-gray-500">
+              Service on: <Clock className="text-purple-400" />
+              <span className="text-black">{booking.time}</span>
+            </h2>
+          </div>
         </div>
-        </div>
-                    
-        ))}
+      ))}
     </div>
-  )
+  );
 }
 
-export default BookingHistoryList
+export default BookingHistoryList;
